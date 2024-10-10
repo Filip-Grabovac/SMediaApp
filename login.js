28175a33aebe680e1bac950878c6686646ddaa17
filip <filip@briliaton.com>
2024-10-10 18:25:20 +0200

    test

--- login.js
+++ login.js
@@ -6,6 +6,8 @@ let eyeHide = document.querySelector('.password-eye-icon.hide');
 const errorMessage = document.querySelector('.error-message');
 const inputs = document.querySelectorAll('.login-input');
 
+console.log('test');
+
 /**
   SHOW/HIDE PASSWORD
   **/
