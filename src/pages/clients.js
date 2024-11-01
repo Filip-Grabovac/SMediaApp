65cc33dceaf7218cf44989a4e01fdb763260acd5
filip <filip@briliaton.com>
2024-11-01 20:29:10 +0100

    fix

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -1,10 +1,11 @@
-import Client from '../Client';
-import User from '../User';
+// import Client from '../Client';
+// import User from '../User';
 
-const client = new Client();
-// const user = new User();
 import User from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/SMediaApp@56dff5fda28e80921ff40eb28e5a7c9e58d4811b/src/User.js';
 import Client from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/SMediaApp@56dff5fda28e80921ff40eb28e5a7c9e58d4811b/src/Client.js';
+
+const client = new Client();
+const user = new User();
 
 let page = 1;
 let per_page = 5;
