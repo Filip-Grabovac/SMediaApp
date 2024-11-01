f7f2512e8e5e752855a1163feff2cb17354289ed
filip <filip@briliaton.com>
2024-11-01 20:10:16 +0100

    Code cleanup

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,5 +1,8 @@
-import User from '../User';
-import Map from '../Map';
+// import User from '../User';
+// import Map from '../Map';
+
+import User from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/SMediaApp@56dff5fda28e80921ff40eb28e5a7c9e58d4811b/src/User.js';
+import Map from 'https://cdn.jsdelivr.net/gh/Filip-Grabovac/SMediaApp@56dff5fda28e80921ff40eb28e5a7c9e58d4811b/src/Map.js';
 
 const user = new User();
 const map = new Map();
