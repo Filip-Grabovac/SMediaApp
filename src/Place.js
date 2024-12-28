be0fb2211b0fe23e277649a390d0b4035fddf526
filip <filip@briliaton.com>
2024-12-28 11:07:19 +0100

    Update

--- src/Place.js
+++ src/Place.js
@@ -99,6 +99,8 @@ export default class Place {
             ? city.tags.wikipedia.split(', ')[1]
             : city.tags.wikipedia.split(', ')[0].replaceAll('en:', '');
 
+        console.log(typeof window.stateInputSearch);
+
         // Return if we are getting border places from another state
         if (
           typeof window.stateInputSearch !== 'undefined' &&
