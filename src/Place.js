65064f189e1eec3fd91d1a0b9f702e8c73499af1
filip <filip@briliaton.com>
2025-01-17 15:54:41 +0100

    Update

--- src/Place.js
+++ src/Place.js
@@ -101,6 +101,7 @@ export default class Place {
     if (citiesWrap && cities.length > 0) {
       cities.forEach((city) => {
         if (!city.tags.wikipedia) {
+            console.log(city);
           return;
         }
 
