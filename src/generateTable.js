f640c00a68c73725b2448b4d78c8d87602acd747
filip <filip@briliaton.com>
2025-03-11 11:53:42 +0100

    Update

--- src/generateTable.js
+++ src/generateTable.js
@@ -361,6 +361,10 @@ $(document).ready(function () {
       const percentage = population / totalPopulation;
       cumulativePercentage += percentage;
 
+      console.log(population);
+      console.log(minPopulation);
+      console.log(maxPopulation);
+
       const normalizedPopulation =
         (population - minPopulation) / (maxPopulation - minPopulation);
       const normalizedAvgIncome =
