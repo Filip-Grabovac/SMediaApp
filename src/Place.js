b87c526792ee8315588dd602e8c9a3f935dd9f9b
filip <filip@briliaton.com>
2025-03-21 13:28:11 +0100

    Update

--- src/Place.js
+++ src/Place.js
@@ -79,7 +79,7 @@ export default class Place {
           query = `
             [out:json];
             (
-              node["place"~"city|town|village|neighbourhood"](${bbox});
+              node["place"~"city|town|village|suburb"](${bbox});
             );
             out body;
           `;
@@ -91,7 +91,7 @@ export default class Place {
         query = `
           [out:json];
           (
-            node["place"~"city|town|village|neighbourhood"](around:${radius}, ${center.lat}, ${center.lng});
+            node["place"~"city|town|village|suburb"](around:${radius}, ${center.lat}, ${center.lng});
           );
           out body;
         `;
@@ -102,7 +102,7 @@ export default class Place {
         query = `
           [out:json];
           (
-            node["place"~"city|town|village|neighbourhood"](${bbox});
+            node["place"~"city|town|village|suburb"](${bbox});
           );
           out body;
         `;
